a09859fa35a65d9792a4601efd9d2ad434266821
Nguyen Nguyen <nguyen.a.nguyen@gmail.com>
2011-06-09 01:18:18 -0500

    #274 Add clean DB capability

--- src/groovy/greenbill/dbstuff/DbCreate.groovy
+++ src/groovy/greenbill/dbstuff/DbCreate.groovy
@@ -14,56 +14,58 @@
  *   limitations under the License.
  */
 package greenbill.dbstuff;
-import groovy.sql.Sql
-import java.sql.SQLException
 
-import org.codehaus.groovy.grails.web.context.ServletContextHolder
-import org.codehaus.groovy.grails.commons.ApplicationHolder
 
-import org.springframework.context.ApplicationContextAware
-import org.springframework.context.ApplicationContext
-	
-import org.apache.ddlutils.io.DatabaseDataIO;
-import org.apache.ddlutils.Platform;
-import org.apache.ddlutils.PlatformFactory;
+import org.apache.commons.logging.LogFactory
+import org.apache.ddlutils.PlatformFactory
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+import org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration
+import org.hibernate.dialect.DialectFactory
+import org.hibernate.tool.hbm2ddl.SchemaExport
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import org.springframework.jdbc.support.JdbcUtils
 
-public class DbCreate {
+public class DbCreate
+{
+  private static final log = LogFactory.getLog(this)
 
-	def dataSource
-	def platform
-	
-	def dropAndCreate(dbname,dsConfig) {
-		 platform = PlatformFactory.createNewPlatformInstance(dataSource)
-		def platformName = platform.name.toLowerCase();
-		if (platformName.contains("mssql")){
-			dropMsSql(dbname,
-				dsConfig.dataSource.driverClassName,dsConfig.dataLoad.createUrl,
-				dsConfig.dataSource.username,dsConfig.dataSource.password)
-			createMsSql(dbname,dsConfig.dataLoad.createDbPath,
-				dsConfig.dataSource.driverClassName,dsConfig.dataLoad.createUrl,
-				dsConfig.dataSource.username,dsConfig.dataSource.password)
-		}else if (platformName.contains("mysql")){
-			dropMySql(dbname)
-			createMySql(dbname)
-		}else if (platformName.contains("hsqldb")){
-			dropHsql(dbname) 
-		}else throw new IllegalArgumentException("Drop and Create not supported for this databse yet")
-	}
-	def create(dbname,dsConfig) {
-		platform = PlatformFactory.createNewPlatformInstance(dataSource)
-		def platformName = platform.name.toLowerCase();
-		if (platformName.contains("mssql")){
-			createMsSql(dbname,dsConfig.dataLoad.createDbPath,
-				dsConfig.dataSource.driverClassName,dsConfig.dataLoad.createUrl,
-				dsConfig.dataSource.username,dsConfig.dataSource.password)
-		}else if (platformName.contains("mysql")){
-			createMySql(dbname)
-		}else if (platformName.contains("HsqlDb")){
-		}else throw new IllegalArgumentException("Create not supported for this databse yet")
-	}
-	
-	def dropMsSql(dbname,driverClassName,url,username,password) {
-		def sql = """
+
+  def dataSource
+  def platform
+
+  def dropAndCreate (dbname, dsConfig) {
+    platform = PlatformFactory.createNewPlatformInstance(dataSource)
+    def platformName = platform.name.toLowerCase();
+    if (platformName.contains("mssql")) {
+      dropMsSql(dbname,
+          dsConfig.dataSource.driverClassName, dsConfig.dataLoad.createUrl,
+          dsConfig.dataSource.username, dsConfig.dataSource.password)
+      createMsSql(dbname, dsConfig.dataLoad.createDbPath,
+          dsConfig.dataSource.driverClassName, dsConfig.dataLoad.createUrl,
+          dsConfig.dataSource.username, dsConfig.dataSource.password)
+    } else if (platformName.contains("mysql")) {
+      dropMySql(dbname)
+      createMySql(dbname)
+    } else if (platformName.contains("hsqldb")) {
+      dropHsql(dbname)
+    } else throw new IllegalArgumentException("Drop and Create not supported for this databse yet")
+  }
+
+  def create (dbname, dsConfig) {
+    platform = PlatformFactory.createNewPlatformInstance(dataSource)
+    def platformName = platform.name.toLowerCase();
+    if (platformName.contains("mssql")) {
+      createMsSql(dbname, dsConfig.dataLoad.createDbPath,
+          dsConfig.dataSource.driverClassName, dsConfig.dataLoad.createUrl,
+          dsConfig.dataSource.username, dsConfig.dataSource.password)
+    } else if (platformName.contains("mysql")) {
+      createMySql(dbname)
+    } else if (platformName.contains("HsqlDb")) {
+    } else throw new IllegalArgumentException("Create not supported for this databse yet")
+  }
+
+  def dropMsSql (dbname, driverClassName, url, username, password) {
+    def sql = """
 				if db_id(\'${dbname}\') is not null
 				BEGIN
 				Alter database ${dbname} set single_user with rollback immediate; 
@@ -71,27 +73,28 @@ public class DbCreate {
 				drop database ${dbname};
 				END
 		"""
-		def ant = new AntBuilder()
-		ant.sql(print:true, autocommit:true, keepformat:true, delimitertype:"row",
-			driver:"${driverClassName}",
-			url:"${url}", userid:"${username}", password:"${password }")
-		{
-				transaction(sql)
-		}
-	}
-	
-	def dropMySql(dbname) {
-		runSql(	"""
+    def ant = new AntBuilder()
+    ant.sql(print: true, autocommit: true, keepformat: true, delimitertype: "row",
+        driver: "${driverClassName}",
+        url: "${url}", userid: "${username}", password: "${password }")
+        {
+          transaction(sql)
+        }
+  }
+
+  def dropMySql (dbname) {
+    runSql("""
 				use mysql;
 				drop database if exists ${dbname};
 		""")
-	}
-	def dropHsql(dbname) {
-	  runSql("DROP SCHEMA PUBLIC CASCADE")
-	}
+  }
 
-	def createMsSql(dbname,path,driverClassName,url,username,password) {
-		def sql="""
+  def dropHsql (dbname) {
+    runSql("DROP SCHEMA PUBLIC CASCADE")
+  }
+
+  def createMsSql (dbname, path, driverClassName, url, username, password) {
+    def sql = """
 			if db_id(\'${dbname}\') is null
 			BEGIN
 			use tempdb;
@@ -102,27 +105,107 @@ public class DbCreate {
 			alter database ${dbname} set recovery simple, auto_shrink on;
 			END
 			"""
-		def ant = new AntBuilder()
-		ant.sql(print:true, keepformat:true, delimitertype:"row",
-				driver:"${driverClassName}",
-				url:"${url}", userid:"${username}", password:"${password }")
-		{
-			transaction(sql)
-		}
-		
-	}
-	def createMySql(dbname) {
-		runSql("create database IF NOT EXISTS ${dbname};")
-	
-	}
-	def runSql(sql) {
-		//try {
-			println sql
-			platform.evaluateBatch(sql,false)
-			//def db = new Sql(dataSource)
-		   	//db.execute(sql)
-		//} catch(Exception e){
-		//	e.printStackTrace()
-		//}
-	}
+    def ant = new AntBuilder()
+    ant.sql(print: true, keepformat: true, delimitertype: "row",
+        driver: "${driverClassName}",
+        url: "${url}", userid: "${username}", password: "${password }")
+        {
+          transaction(sql)
+        }
+
+  }
+
+  def createMySql (dbname) {
+    runSql("create database IF NOT EXISTS ${dbname};")
+
+  }
+
+  def runSql (sql) {
+    //try {
+    println sql
+    platform.evaluateBatch(sql, false)
+    //def db = new Sql(dataSource)
+    //db.execute(sql)
+    //} catch(Exception e){
+    //	e.printStackTrace()
+    //}
+  }
+
+  def create (grailsApplication) {
+    def props = populateProperties()
+
+    GrailsAnnotationConfiguration config =
+    new GrailsAnnotationConfiguration();
+    config.setGrailsApplication(grailsApplication)
+    config.setProperties(props)
+
+    def classLoader = grailsApplication.classLoader
+    def eventsClassLoader = new GroovyClassLoader(classLoader)
+    def hibernateCfgXml = eventsClassLoader.getResource('hibernate.cfg.xml')
+    if (hibernateCfgXml) {
+      config.configure(hibernateCfgXml)
+    }
+
+    new SchemaExport(config).create(false, true)
+  }
+
+  def populateProperties = {
+    def props = new Properties()
+
+    def dsConfig = ConfigurationHolder.config
+
+    props.'hibernate.connection.username' = dsConfig?.dataSource?.username ?: 'sa'
+    props.'hibernate.connection.password' = dsConfig?.dataSource?.password ?: ''
+    props.'hibernate.connection.url' = dsConfig?.dataSource?.url ?: 'jdbc:hsqldb:mem:testDB'
+    props.'hibernate.connection.driver_class' =
+      dsConfig?.dataSource?.driverClassName ?: 'org.hsqldb.jdbcDriver'
+
+    if (dsConfig?.dataSource?.configClass) {
+      if (dsConfig.dataSource.configClass instanceof Class) {
+        configClassName = dsConfig.dataSource.configClass.name
+      }
+      else {
+        configClassName = dsConfig.dataSource.configClass
+      }
+    }
+
+    def namingStrategy = dsConfig?.hibernate?.naming_strategy
+    if (namingStrategy) {
+      try {
+        GrailsDomainBinder.configureNamingStrategy namingStrategy
+      }
+      catch (Throwable t) {
+        log.warn """WARNING: You've configured a custom Hibernate naming strategy '$namingStrategy' in DataSource.groovy, however the class cannot be found.
+              Using Grails' default naming strategy: '${GrailsDomainBinder.namingStrategy.getClass().name}'"""
+      }
+    }
+
+    if (dsConfig?.dataSource?.dialect) {
+      def dialect = dsConfig.dataSource.dialect
+      if (dialect instanceof Class) {
+        dialect = dialect.name
+      }
+      props.'hibernate.dialect' = dialect
+    }
+    else {
+      log.info 'NOTICE: Autodetecting the Hibernate Dialect; consider specifying the class name in DataSource.groovy'
+      try {
+        def ds = new DriverManagerDataSource(
+            props.'hibernate.connection.driver_class',
+            props.'hibernate.connection.url',
+            props.'hibernate.connection.username',
+            props.'hibernate.connection.password')
+        def dbName = JdbcUtils.extractDatabaseMetaData(ds, 'getDatabaseProductName')
+        def majorVersion = JdbcUtils.extractDatabaseMetaData(ds, 'getDatabaseMajorVersion')
+        props.'hibernate.dialect' =
+          DialectFactory.determineDialect(dbName, majorVersion).class.name
+      }
+      catch (Exception e) {
+        log.error "ERROR: Problem autodetecting the Hibernate Dialect: ${e.message}"
+        throw e
+      }
+    }
+
+    return props
+  }
 }
